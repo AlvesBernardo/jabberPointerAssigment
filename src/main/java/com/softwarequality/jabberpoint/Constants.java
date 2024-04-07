91c552e7eb1e4cd28a04ec21b3ca029953eecc82
AlvesBernardo <bernardo.correia.alves@student.nhlstenden.com>
2024-04-07 23:15:36 +0200

    Fix constants.json not loading

--- src/main/java/com/softwarequality/jabberpoint/Constants.java
+++ src/main/java/com/softwarequality/jabberpoint/Constants.java
@@ -1,39 +1,42 @@
 package com.softwarequality.jabberpoint;
 
 import org.json.JSONObject;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class Constants {
-    private JSONObject constants;
+    private final JSONObject constants;
 
     public Constants(String filePath) {
-        try {
-            InputStream is = getClass().getClassLoader().getResourceAsStream(filePath);
-            if (is == null) {
-                throw new IllegalArgumentException("File " + filePath + " not found!");
-            } else {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-                String line;
-                StringBuilder contentBuilder = new StringBuilder();
-                while ((line = reader.readLine()) != null) {
-                    contentBuilder.append(line).append("\n");
-                }
-
-                reader.close();
-
-                this.constants = new JSONObject(contentBuilder.toString());
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        String content = "";
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream(filePath)) { // If your file is at the root of the resources directory
+             if (is == null) {
+            throw new IllegalArgumentException("File " + filePath + " not found!");
         }
-    }
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+            String line;
+            StringBuilder contentBuilder = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                contentBuilder.append(line).append("\n");
+            }
+            content = contentBuilder.toString();
+        }
+    } catch(
+    IOException e)
 
-    public String getValue(String key) {
-        return this.constants.getString(key);
+    {
+        e.printStackTrace();  // Consider logging this exception or handling it in a way that's suitable for your application.
     }
+        this.constants =new
+
+    JSONObject(content);
+}
+
+public String getValue(String key) {
+    return this.constants.getString(key);
+}
 }
